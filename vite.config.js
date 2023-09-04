adeb51b35646dfb949bdfcaacccbc33b1ef818cf
mhio <37205532+mhio@users.noreply.github.com>
2023-09-04 05:15:48 +0000

    tone down the es output from esbuild. (es2020->es2017)

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 export default defineConfig({
+  build: {
+    target: 'es2017',
+  },
   plugins: [
     vue(),
     vuetify({
