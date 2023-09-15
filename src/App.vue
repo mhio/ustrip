a4ea9bf3e8f045f4c186a6ce6733a2700e4043bd
mhio <37205532+mhio@users.noreply.github.com>
2023-09-15 07:58:57 +0000

    alt link and buttons

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,25 @@
         <v-card-text>
           <p class="mb-4">Remove <code>utm_</code> like tracking query paramaters from a URL</p>
           <v-text-field density="compact" label="URL" v-model="link"></v-text-field>
-          <v-text-field density="compact" label="Fixed" v-model="fixed_link"></v-text-field>
-          <div class="d-flex mb-4">
-            <v-btn raised :href="fixed_link" target="_blank" class="mr-2">Open</v-btn>
-            <v-btn raised @click="copyText" class="ml-2">Copy</v-btn>
+          <v-text-field density="compact" label="Fixed" v-model="fixed_link">
+            <template v-slot:append>
+              <v-btn raised :href="fixed_link" target="_blank" class="mr-1" icon density="comfortable">
+                <v-icon icon="mdi-open-in-new"/>
+              </v-btn>
+              <v-btn raised @click="copyText" class="ml-1" icon density="comfortable"><v-icon icon="mdi-content-copy"/></v-btn>
+            </template> 
+          </v-text-field>
+          <div class="d-flex">
+            <div class="flex-grow-1"><v-text-field density="compact" label="Alt" v-model="alt_link">
+              <template v-slot:append>
+                <v-btn raised :href="alt_link" target="_blank" class="mr-1" icon density="comfortable">
+                  <v-icon icon="mdi-open-in-new"/>
+                </v-btn>
+                <v-btn raised @click="copyTextAlt" class="ml-1" icon density="comfortable"><v-icon icon="mdi-content-copy"/></v-btn>
+              </template> 
+             </v-text-field></div>
           </div>
-          <p><b>Open</b> the fixed URL in a new window or <b>Copy</b> fixed to the clipboard</p>
+          <p class="mb-4"><v-icon class="mr-1" icon="mdi-open-in-new"/><b>Open</b> the fixed URL in a new window or <v-icon class="mr-1" icon="mdi-content-copy"/><b>Copy</b> the URL to the clipboard</p>
         </v-card-text>
       </v-card>
     </v-main>
@@ -21,10 +34,22 @@
 </template>
 
 <script setup>
-import { ref, watch, onMounted } from 'vue'
+import { ref, watch, onMounted, computed } from 'vue'
 const link = ref(null)
 const fixed_link = ref(null)
-
+const alt_link = computed(() =>{
+  if (!fixed_link.value) return ''
+  const str_link = String(fixed_link.value)
+  if (
+    str_link.startsWith('https://x.com/') 
+    || str_link.startsWith('x.com/')
+    || str_link.startsWith('https://twitter.com/')
+    || str_link.startsWith('twitter.com/')
+  ){
+    return str_link.replace(/[a-z0-9-]+?\.com\//i, 'nitter.net/')
+  }
+  return str_link
+})
 const git_commit = __GIT_COMMIT__
 
 watch(link, () => {
@@ -70,6 +95,9 @@ function fixLink(url){
 }
 function copyText() {
   navigator.clipboard.writeText(fixed_link.value)
+}
+function copyTextAlt() {
+  navigator.clipboard.writeText(alt_link.value)
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,20 +1,22 @@
 import { createApp } from 'vue'
 import { createVuetify } from 'vuetify'
-import { mdi } from 'vuetify/iconsets/mdi-svg'
+//import { mdi, aliases } from 'vuetify/iconsets/mdi'
 import "@fontsource/roboto/400.css"
 //import '@mdi/font/css/materialdesignicons.css'
+import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
 import App from './App.vue'
 
 
 const vuetify = createVuetify({
-  icons: {
-    defaultSet: 'mdi',
-    sets: {
-      mdi,
-    },
-  }
+  //icons: {
+  //  defaultSet: 'mdi',
+  //  aliases,
+  //  sets: {
+  //    mdi,
+  //  },
+  //}
 })
 
 
